6d367fe9f49d72d21f6a094664a091f4b7694fc3
Dinu98 <alexdinescu888@gmail.com>
2025-02-10 20:55:59 +0200

    Company model

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
-console.log(`hi there`);
+import { User } from './models/User';
+import { Company } from './models/Company';
+
+const user = new User();
+const company = new Company();
+
+console.log(user);
+console.log(company);
