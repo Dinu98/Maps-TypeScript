00563d81bca3239e0c0272680cc1b6460151cb42
Dinu98 <alexdinescu888@gmail.com>
2025-02-10 21:18:58 +0200

    Showing map on the screen

--- src/index.ts
+++ src/index.ts
@@ -6,3 +6,13 @@ const company = new Company();
 
 console.log(user);
 console.log(company);
+
+new google.maps.Map(document.getElementById('map') as HTMLElement, 
+  {
+    zoom: 1,
+    center: {
+      lat: 0,
+      lng: 0
+    }
+  }
+); 
